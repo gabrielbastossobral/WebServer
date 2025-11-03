b5f27252790612ce105550e22b7d0a9cd185c2a7
Gabriel Costa Matos <gcosta-m@c2r6p3.42sp.org.br>
2025-11-03 16:32:03 -0300

    continue config

--- webServ_42/includes/Config_Parser.hpp
+++ webServ_42/includes/Config_Parser.hpp
@@ -6,7 +6,7 @@
 /*   By: gcosta-m <gcosta-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 10:31:25 by gcosta-m          #+#    #+#             */
-/*   Updated: 2025/10/21 10:31:26 by gcosta-m         ###   ########.fr       */
+/*   Updated: 2025/11/03 10:24:03 by gcosta-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,8 @@ class Config_Parser
 	Server parse_server(size_t *index);
 	Location parse_location(size_t *index);
 
-	int set_server_value(Server *server, const std::string &key,
-		const std::string &value);
-	int set_location_value(Location *location, const std::string &key,
-		const std::string &value);
-
+	int set_server_value(Server *server, const std::string &key, const std::string &value);
+	int set_location_value(Location *location, const std::string &key,const std::string &value);
 	int check_line_syntax(const std::string &line);
 	int print_parse_error();
 
--- webServ_42/srcs/Config_Parser.cpp
+++ webServ_42/srcs/Config_Parser.cpp
@@ -6,7 +6,7 @@
 /*   By: gcosta-m <gcosta-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 10:31:29 by gcosta-m          #+#    #+#             */
-/*   Updated: 2025/10/21 10:31:30 by gcosta-m         ###   ########.fr       */
+/*   Updated: 2025/11/03 14:26:18 by gcosta-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,72 @@ Config_Parser::Config_Parser(const char *filename)
 	}
 }
 
-Config_Parser::~Config_Parser()
+Config_Parser::~Config_Parser(){}
+
+std::vector<Server> *Config_Parser::parse()
 {
+	std::vector<Server> *servers = new std::vector<Server>;
+
+	std::cout << "Starting config parsing..." << std::endl;
+	size_t pre = 0;
+	size_t cur = content.find_first_not_of(" \n\t");
+
+	while (cur != std::string::npos)
+	{
+		pre = content.find_first_not_of(" \n\t", cur);
+		cur = content.find_first_of(" \n\t", pre);
+		std::string token = content.substr(pre, cur - pre);
+		if (token != "server")
+		{
+			print_parse_error();
+			delete servers;
+			return NULL;
+		}
+		Server server = parse_server(&cur);
+		servers->push_back(server);
+	}
+	std::cout << "Config parsing completed successfully." << std::endl;
+	return servers;
+}
+
+Server Config_Parser::parse_server(size_t *index)
+{
+	Server server;
+	size_t pre = content.find_first_not_of(" \n\t", *index);
+	if (pre == std::string::npos || content[pre] != '{')
+	{
+		print_parse_error();
+		return server;
+	}
+	*index++;
+	while (true)
+	{
+		pre = content.find_first_not_of(" \n\t", *index);
+		if (pre == std::string::npos)
+		{
+			print_parse_error();
+			return server;
+		}
+		if (content[pre] == '}')
+		{
+			*index = content.find_first_not_of(" \n\t", pre + 1);
+			break;
+		}
+		size_t key_end = content.find_first_of(" \n\t;", pre);
+		if (key_end == std::string::npos)
+		{
+			print_parse_error();
+			return server;
+		}
+		std::string key = content.substr(pre, key_end - pre);
+		pre = key;
+		if (key == "location")
+		{
+			server.locations.push_back(parse_location(index));
+			continue;
+		}
+		pre = content.find_first_not_of(" \n\t", pre);
+		if (pre == )
+	}
+	
 }
