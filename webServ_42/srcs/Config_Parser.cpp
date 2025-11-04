13ab581cd39de50492ac860da2df86b6ef7610cf
Gabriel Costa Matos <gcosta-m@c2r5p3.42sp.org.br>
2025-11-04 10:34:27 -0300

    add parse functions

--- webServ_42/srcs/Config_Parser.cpp
+++ webServ_42/srcs/Config_Parser.cpp
@@ -6,11 +6,11 @@
 /*   By: gcosta-m <gcosta-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 10:31:29 by gcosta-m          #+#    #+#             */
-/*   Updated: 2025/11/03 14:26:18 by gcosta-m         ###   ########.fr       */
+/*   Updated: 2025/11/04 10:33:03 by gcosta-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Config_Parser.hpp"
+#include "../includes/Config_Parser.hpp"
 
 Config_Parser::Config_Parser(const char *filename)
 {
@@ -65,41 +65,91 @@ std::vector<Server> *Config_Parser::parse()
 Server Config_Parser::parse_server(size_t *index)
 {
 	Server server;
+	size_t key_start;
+	size_t value_end;
+
 	size_t pre = content.find_first_not_of(" \n\t", *index);
 	if (pre == std::string::npos || content[pre] != '{')
+		exit(print_parse_error());
+
+	pre++;
+	size_t cur = content.find_first_not_of(" \n\t", pre);
+	while(cur != std::string::npos)
 	{
-		print_parse_error();
-		return server;
-	}
-	*index++;
-	while (true)
-	{
-		pre = content.find_first_not_of(" \n\t", *index);
-		if (pre == std::string::npos)
+		if((pre = content.find_first_not_of(" \n\t", cur)) == std::string::npos)
+			exit(print_parse_error());
+		key_start = pre;
+		if ((cur = content.find_first_of(" \n\t", key_start)) == std::string::npos)
+			exit(print_parse_error());
+		std::string key = content.substr(key_start, cur - key_start);
+		if (key == "}")
 		{
-			print_parse_error();
-			return server;
-		}
-		if (content[pre] == '}')
-		{
-			*index = content.find_first_not_of(" \n\t", pre + 1);
+			*index = content.find_first_not_of(" \n\t", cur + 1);
 			break;
 		}
-		size_t key_end = content.find_first_of(" \n\t;", pre);
-		if (key_end == std::string::npos)
-		{
-			print_parse_error();
-			return server;
-		}
-		std::string key = content.substr(pre, key_end - pre);
-		pre = key;
 		if (key == "location")
 		{
-			server.locations.push_back(parse_location(index));
-			continue;
+			server.add_location(parse_location(&cur));
 		}
-		pre = content.find_first_not_of(" \n\t", pre);
-		if (pre == )
+		else
+		{
+			if ((pre = content.find_first_not_of(" \n\t", cur)) == std::string::npos)
+				exit(print_parse_error());
+			if ((cur = content.find_first_of(" \n\t;", pre)) == std::string::npos)
+				exit(print_parse_error());
+			if ((value_end = content.find_first_not_of(" \n\t", cur + 1)) == std::string::npos)
+				exit(print_parse_error());
+			if ((int)value_end == EMPTY)
+				continue;
+			std::string value = content.substr(pre, value_end - pre + key_start + 1);
+			if (set_server_value(&server, key, value) == FAILURE)
+				exit(print_parse_error());
+		}
 	}
-	
+	return server;
+}
+
+Location Config_Parser::parse_location(size_t *index)
+{
+	Location location;
+	size_t key_start;
+	size_t value_end;
+
+	size_t pre = content.find_first_not_of(" \n\t", *index);
+	size_t cur = content.find_first_of(" \n\t", pre);
+	location.path = content.substr(pre, cur - pre);
+
+	pre = content.find_first_not_of(" \n\t", cur);
+	if (pre == std::string::npos || content[pre] != '{')
+		exit(print_parse_error());
+	pre++;
+	cur = content.find_first_not_of(" \n\t", pre);
+	while(cur != std::string::npos)
+	{
+		if ((pre = content.find_first_not_of(" \n\t", cur)) == std::string::npos)
+			exit(print_parse_error());
+		key_start = pre;
+		if ((cur = content.find_first_of(" \n\t", key_start)) == std::string::npos)
+			exit(print_parse_error());
+		std::string key = content.substr(pre, cur - pre);
+		if (key == "}")
+		{
+			*index = cur;
+			break;
+		}else
+		{
+			if ((pre = content.find_first_not_of(" \n\t", cur)) == std::string::npos)
+				exit(print_parse_error());
+			if ((cur = content.find_first_of(" \n\t;", pre)) == std::string::npos)
+				exit(print_parse_error());
+			if ((value_end = check_line_syntax(content.substr(key_start, cur - key_start))) == FAILURE)
+				exit(print_parse_error());
+			if ((int)value_end == EMPTY)
+				continue;
+			std::string value = content.substr(pre, value_end - pre + key_start + 1);
+			if (set_location_value(&location, key, value) == FAILURE)
+				exit(print_parse_error());
+		}
+	}
+	return location;
 }
